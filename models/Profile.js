b0b1d1dea776c7f91a8e14767197a2b21136763e
DILEEP REDDY <saidileepreddyaella@gmail.com>
2021-07-03 22:55:05 +0530

    Add profile experience

--- models/Profile.js
+++ models/Profile.js
@@ -42,7 +42,7 @@ const ProfileSchema = new mongoose.Schema({
         type: String,
       },
       from: {
-        type: Date,
+        type: String,
         required: true
       },
       to: {
